4673eabd75254e8cfa1035e16acb2d31a37df33c
clincher <va.bolshakov@gmail.com>
2012-05-12 02:27:57 +0400

    email changed, socials links removed, metrika added, news added to menu

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-# Django settings for susmet project.
+# Django settings for temp-msk.ru project.
 
 import os
 DEBUG = True
@@ -10,16 +10,16 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = [('Vasiliy', 'va.bolshakov@gmail.com'),]
 
-MANAGERS = ADMINS + [('Sokolov', 'temp7_7@mail.ru'),]
+MANAGERS = ADMINS + [('Sokolov', 'info@temp-msk.ru'),]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'temp.db',                      # Or path to database file if using sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'temp.db',
+        'HOST': '',
+        'USER': '',
+        'PASSWORD': '',
+        'PORT': '',
     }
 }
 
